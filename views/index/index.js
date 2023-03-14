a03ba4648575e0f13d8714bafea57be8b85dec71
MujaffarYadawad <mujaffaryadawad587313@gmail.com>
2023-03-14 13:58:14 +0530

    setinterval to get all msg without refreshig page

--- views/index/index.js
+++ views/index/index.js
@@ -1,9 +1,64 @@
-console.log('index chats')
+//console.log('index chats')
+
+ 
 const url = `http://localhost:3000`;
 
 
+const token = localStorage.getItem('token')
+//console.log('token--', token)
+  //const parentNode = document.getElementById("chatMsg");
+
+
+     
+
+window.addEventListener("DOMContentLoaded",   getMsg)
+
+async function showMsgOnScreen(data) {
+  //   console.log('data', data )
+  //  for (const key in data) {
+  //   // console.log( data[key].message);
+  //    let i = data[key].message;
+  //    console.log(i)
+  //      const parentNode = document.getElementById("chat-msg");
+  //      const childHTML = `<li class="chat-msg-li">${i}</li>`;
+  //      parentNode.innerHTML += childHTML;
+  //  }
+  //  const parentNode = document.getElementById("chatMsg");
+  //  parentNode.innerHTML = " ";
+
+  // const chatMsgLi = document.createElement("li");
+  // chatMsgLi.classList.add("chat-msg-li");
+  // chatMsgLi.textContent = data.message;
+  // parentNode.appendChild(chatMsgLi);
+
+  const parentNode = document.getElementById("chatMsg");
+  const childHTML = `<li class="chat-msg-li">${data.message}</li>`;
+  parentNode.innerHTML= parentNode.innerHTML + childHTML;
+}
+
+async function getMsg(){
+  //console.log(parentNode);
+   
+   try {
+   
+     const res = await axios.get(`${url}/message/getMessage`, {
+       headers: { Authorization: token },
+     });
+
+     console.log("getMessges-->", res);
+     
+
+     for (let i = 0; i < res.data.length; i++) {
+       showMsgOnScreen(res.data[i]);
+     }
+   } catch (err) {
+     console.log(err);
+   }
+ 
+}
+
 async function sendMessage(event){
- // console.log('index ffunct')
+  console.log('index ffunct')
   event.preventDefault();
   const msg = event.target.message.value;
   const msgs = {
@@ -13,50 +68,29 @@ async function sendMessage(event){
     const res = await axios.post(`${url}/message/postMessage`, msgs, 
     { headers: { Authorization: token } 
   })
+   event.target.message.value = ""; 
     console.log(res)
-       
+     
+
   } catch (err) {
     console.log(err)
     
-  }
   
 }
-
-const token = localStorage.getItem("token");
-//console.log('token--', token)
-
-console.log("loadeing  n diom");
-window.addEventListener("DOMContentLoaded", async () => {
-  try {
-    console.log("loadeing diom");
-    const res = await axios.get(`${url}/message/getMessage`, {
-      headers: { Authorization: token },
-    });
-   // console.log("getMessges-->", res);
-
-    for (let i = 0; i < res.data.length; i++) {
-      showMsgOnScreen(res.data[i]);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-
-
-async function showMsgOnScreen(data) {
-   
-//   console.log('data', data )
-//  for (const key in data) {
-//   // console.log( data[key].message);
-//    let i = data[key].message;
-//    console.log(i)
-//      const parentNode = document.getElementById("chat-msg");
-//      const childHTML = `<li class="chat-msg-li">${i}</li>`;
-//      parentNode.innerHTML += childHTML;
-//  }
-       const parentNode = document.getElementById("chat-msg");
-       const childHTML = `<li class="chat-msg-li">${data.message}</li>`; // if u are using above for loop then use i insted of data.message
-       parentNode.innerHTML += childHTML;
-
 }
+
+ setInterval(()=> {
+    const parentNode = document.getElementById("chatMsg");
+    parentNode.innerHTML = " ";
+   getMsg()
+ }, 3000)
+ 
+
+
+
+
+
+
+
+
+ 
